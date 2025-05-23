bfdde1a49ac5328b2d54bf55d24d06026243bd5e
Csing102 <csing102@eq.edu.au>
2025-05-24 05:16:33 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const chatLog = document.getElementById('chat-log');
+const chatInput = document.getElementById('chat-input');
+const chatButton = document.getElementById('chat-button');
 const uploadInput = document.getElementById('upload-input');
 const uploadButton = document.getElementById('upload-button');
 const trainButton = document.getElementById('train-button');
@@ -21,11 +24,33 @@ trainButton.addEventListener('click', () => {
     method: 'POST',
     body: formData
   })
- .then(response => response.json())
- .then(data => {
+.then(response => response.json())
+.then(data => {
     uploadStatus.innerText = `Training complete: ${data.message}`;
   })
- .catch(error => {
+.catch(error => {
     uploadStatus.innerText = `Error: ${error.message}`;
   });
+});
+
+chatButton.addEventListener('click', () => {
+  const message = chatInput.value.trim();
+  if (message) {
+    const response = fetch('/chat', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ message })
+    })
+  .then(response => response.json())
+  .then(data => {
+      chatLog.innerHTML += `<div><b>You:</b> ${message}</div>`;
+      chatLog.innerHTML += `<div><b>Chatbot:</b> ${data.response}</div>`;
+      chatInput.value = '';
+    })
+  .catch(error => {
+      chatLog.innerHTML += `<div><b>Error:</b> ${error.message}</div>`;
+    });
+  }
 });
